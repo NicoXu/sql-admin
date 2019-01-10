09a5efaad3796c1c132c919ee68d53159746ef8c
Nico <nicoxu1994@outlook.com>
2019-01-10 22:49:32 +0800

    login页面

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import 'ant-design-vue/dist/antd.css'
+import Antd from 'ant-design-vue'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(Antd)
 
 /* eslint-disable no-new */
 new Vue({

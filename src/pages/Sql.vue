840790a79372ebbe502ee5796cbb6c35be8869ce
Nico <nicoxu1994@outlook.com>
2019-01-31 00:23:02 +0800

    sqlList组件修改

--- src/pages/Sql.vue
+++ src/pages/Sql.vue
@@ -18,7 +18,7 @@
       <a-layout-header :style="{ background: '#fff', padding: 0 }" />
       <a-layout-content :style="{ margin: '24px 16px 0' }">
         <div :style="{ padding: '24px', background: '#fff', minHeight: '900px' }">
-          
+            <sql-list></sql-list>
         </div>
       </a-layout-content>
       <a-layout-footer style="textAlign: center">
@@ -29,8 +29,10 @@
 </template>
 <script>
 import Header from '../components/Header'
+import SqlList from '../components/SqlList'
 export default {
     components: {Header},
+    components: {SqlList},
     name: 'Sql',  
     methods: {
         onCollapse(collapsed, type) {

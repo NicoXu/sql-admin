7686c0ca5beb5f1df361d2b81a777053822cd2fb
shaw <nicoxu1994@outlook.com>
2019-02-14 17:31:52 +0800

    20190214更新
    20190214更新

--- src/components/SqlList.vue
+++ src/components/SqlList.vue
@@ -9,31 +9,134 @@
       >
         Reload
       </a-button>-->
-      <span style="margin-left: 8px">
-        <template v-if="hasSelected">{{`Selected ${selectedRowKeys.length} items`}}</template>
-      </span>
+      <div class="components-input-demo-size">
+        <a-form class="ant-advanced-search-form" :form="form" @submit="query">
+          <a-row>
+            <a-col :span="8">
+              <a-form-item label="版本号">
+                <a-input/>
+              </a-form-item>
+            </a-col>
+            <a-col :span="8">
+              <a-form-item label="脚本类型">
+                <a-select style="width: 200px">
+                  <a-select-option value="DEV">配置数据变更</a-select-option>
+                  <a-select-option value="SIT">业务数据变更</a-select-option>
+                  <a-select-option value="FT">结构变更</a-select-option>
+                </a-select>
+              </a-form-item>
+            </a-col>
+            <a-col :span="8">
+              <a-form-item label="作者">
+                <a-select style="width: 200px">
+                  <a-select-option value="xule">徐乐</a-select-option>
+                  <a-select-option value="zhaokaining">赵凯宁</a-select-option>
+                  <a-select-option value="wuzelin">吴泽林</a-select-option>
+                  <a-select-option value="xiedong">谢栋</a-select-option>
+                  <a-select-option value="luqiang">卢强</a-select-option>
+                  <a-select-option value="shaorongjun">邵荣俊</a-select-option>
+                  <a-select-option value="zhuhaocheng">朱浩成</a-select-option>
+                  <a-select-option value="zangyanyan">臧延岩</a-select-option>
+                  <a-select-option value="fengjianlong">冯建龙</a-select-option>
+                  <a-select-option value="zhangxinbiao">张新标</a-select-option>
+                  <a-select-option value="wangxuan">王炫</a-select-option>
+                  <a-select-option value="wangdeli">王得利</a-select-option>
+                  <a-select-option value="yanwang">颜旺</a-select-option>
+                  <a-select-option value="limeng">李蒙</a-select-option>
+                  <a-select-option value="liupengbo">刘鹏博</a-select-option>
+                  <a-select-option value="yuansili">袁思丽</a-select-option>
+                  <a-select-option value="wangxiaojuan">王晓娟</a-select-option>
+                </a-select>
+              </a-form-item>
+            </a-col>
+          </a-row>
+          <a-row>
+            <a-col :span="8">
+              <a-form-item label="需求号">
+                <a-input/>
+              </a-form-item>
+            </a-col>
+            <a-col :span="8">
+              <a-form-item label="使用环境">
+                <a-select style="width: 200px">
+                  <a-select-option value="DEV">开发环境</a-select-option>
+                  <a-select-option value="SIT">测试环境</a-select-option>
+                  <a-select-option value="FT">FT环境</a-select-option>
+                  <a-select-option value="PRODUCT">生产环境</a-select-option>
+                </a-select>
+              </a-form-item>
+            </a-col>
+            <a-col :span="8">
+              <a-form-item label="有效">
+                <a-select style="width: 200px" defaultValue="1">
+                  <a-select-option value="1">有效</a-select-option>
+                  <a-select-option value="2">无效</a-select-option>
+                </a-select>
+              </a-form-item>
+            </a-col>
+          </a-row>
+        </a-form>
+      </div>
+
+      <a-button type="primary" icon="search">查询</a-button>
+      <a-button icon="edit">修改</a-button>
+      <a-button type="danger" icon="delete">删除</a-button>
+      <a-button type="primary" icon="file-add">新增</a-button>
+      <a-button icon="caret-down" @click="showDownloadModal">下载</a-button>
+
     </div>
-    <a-table
-      :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
-      :columns="columns"
-      :dataSource="data"
+    <a-table :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}" :columns="columns" :dataSource="data"
     />
+    <div>
+      <!--<a-button type="primary" @click="showModal">Open Modal with async logic</a-button>-->
+      <a-modal title="Title" :visible="visible" @ok="handleOk" :confirmLoading="confirmLoading" @cancel="handleCancel">
+        <p>{{ModalText}}</p>
+      </a-modal>
+    </div>
   </div>
 </template>
 
 <script>
 const columns = [
   {
-    title: "Name",
-    dataIndex: "name"
+    title: "版本号",
+    dataIndex: "version"
   },
   {
-    title: "Age",
-    dataIndex: "age"
+    title: "需求号",
+    dataIndex: "requirementNumber"
   },
   {
-    title: "Address",
+    title: "脚本类型",
+    dataIndex: "type"
+  },
+  {
+    title: "脚本",
     dataIndex: "address"
+  },
+  {
+    title: "环境",
+    dataIndex: "enviroment"
+  },
+  {
+    title: '有效',
+    dataIndex: "isValid"
+  },
+  {
+    title: '作者',
+    dataIndex: "author"
+  },
+  {
+    title: "备注",
+    dataIndex: "comment"
+  },
+  {
+    title: "创建时间",
+    dataIndex: "createDate"
+  },
+  {
+    title: "更新时间",
+    dataIndex: "updateDate"
   }
 ];
 
@@ -41,9 +144,15 @@ const data = [];
 for (let i = 0; i < 46; i++) {
   data.push({
     key: i,
-    name: `Edward King ${i}`,
-    age: 32,
-    address: `London, Park Lane no. ${i}`
+    version: `20180222 ${i}`,
+    requirementNumber: `REQ7412399 ${i}`,
+    type: `CONFIG_DML ${i}`,
+    enviroment: 'sit',
+    isValid: '1',
+    author: 'XULE',
+    comment: '这是测试，sql脚本都是乱写的',
+    createDate: "2019-02-14 10:52:00",
+    updateDate: "2019-02-14 10:52:00"
   });
 }
 export default {
@@ -73,11 +182,24 @@ export default {
     onSelectChange(selectedRowKeys) {
       console.log("selectedRowKeys changed: ", selectedRowKeys);
       this.selectedRowKeys = selectedRowKeys;
+    },
+    query() {
+
+    },
+    showDownloadModal() {
+      
     }
   }
 };
 </script>
 
 <style lang="less" scoped>
+  .components-input-demo-size .ant-input {
+    width: 200px;
+    margin: 0 8px 8px 0;
+  }
+  
+  .ant-advanced-search-form .ant-form-item {
+    display: flex;
+  }
 </style>
-
